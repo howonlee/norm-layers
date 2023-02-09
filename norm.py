816f02f20855397e5282543f198f6bf6ce0116ca
Howon Lee <hlee.howon@gmail.com>
2023-02-08 22:47:06 -0800

    l2

--- norm.py
+++ norm.py
@@ -1,16 +1,18 @@
 import numpy as np
 import numpy.random as npr
 
-def norm(var):
+# first do L2 norm, then do L1 norm...
+
+def l2_norm(var):
     return var / var.sum()
 
-def dnorm(var):
+def dl2_norm(var):
     return (var.sum() - (var ** 2)) / (var.sum() ** 2)
 
 if __name__ == "__main__":
     npr.seed(1337)
     init = npr.rand(100)
     h = np.zeros(100) + 1e-3
-    print(norm(init))
-    print(dnorm(init))
-    print((norm(init + h) - norm(init - h)) / (2 * h))
+    print(l2_norm(init))
+    print(dl2_norm(init))
+    print((l2_norm(init + h) - l2_norm(init - h)) / (2 * h))
