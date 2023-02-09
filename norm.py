a3cbf8959113e214906f34b12ed4cad472c66787
Howon Lee <hlee.howon@gmail.com>
2023-02-08 22:43:51 -0800

    it changes fuck

--- norm.py
+++ norm.py
@@ -4,9 +4,13 @@ import numpy.random as npr
 def norm(var):
     return var / var.sum()
 
+def dnorm(var):
+    return (var.sum() - (var ** 2)) / (var.sum() ** 2)
+
 if __name__ == "__main__":
     npr.seed(1337)
     init = npr.rand(100)
-    h = np.zeros(100) + 1e-2
-    print(init)
+    h = np.zeros(100) + 1e-3
+    print(norm(init))
+    print(dnorm(init))
     print((norm(init + h) - norm(init - h)) / (2 * h))
