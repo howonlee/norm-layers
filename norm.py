99f5776b4ac3baa4df57ac66543dedbd66855983
Howon Lee <hlee.howon@gmail.com>
2023-02-11 21:43:11 -0800

    ok, wrong delta for norm 1

--- norm.py
+++ norm.py
@@ -14,16 +14,14 @@ def dl2_norm(var, dout):
     return (((dout * norm) - (var_dout_reduced * var)) / (norm ** 2))
 
 def l1_norm(var):
-    #####
-    #####
-    #####
-    pass
+    norm = np.sum(var)
+    return var / norm
 
-def dl1_norm(var):
-    #####
-    #####
-    #####
-    pass
+def dl1_norm(var, dout):
+    norm = np.sum(var)
+    var_dout_reduced = np.sum(var * dout)
+    var_dout_reduced /= norm
+    return (((dout * norm) - (var_dout_reduced * var)) / (norm ** 2))
 
 def putative_vjp(fn, var, h):
     return (fn(var + h) - fn(var - h)) / (2 * h)
@@ -35,3 +33,9 @@ if __name__ == "__main__":
     print(l2_norm(init))
     print(dl2_norm(init, np.ones(100)))
     print(putative_vjp(l2_norm, init, h))
+    print("=============")
+    print("=============")
+    print("=============")
+    
+    print(dl1_norm(init, np.ones(100)))
+    print(putative_vjp(l1_norm, init, h))
